ee39b99e99c50f7e16743f9a3c98c9a948da4448
Luis <luisjapadilla@gmail.com>
2022-05-14 12:07:26 -0500

    commit

---
+++ Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target, target2;
+    public GameObject playeractive;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            if (playeractive.activeSelf)
+            {
+                Vector3 targetposition = new Vector3(target.position.x,
+                target.position.y,
+               transform.position.z);
+            targetposition.x = Mathf.Clamp(targetposition.x,
+                minPosition.x,
+                maxPosition.x);
+
+            targetposition.y = Mathf.Clamp(targetposition.y,
+                minPosition.y,
+                maxPosition.y);
+            transform.position = Vector3.Lerp(transform.position, targetposition, smoothing);
+            }
+            else
+            {
+                Vector3 targetposition = new Vector3(target2.position.x,
+               target2.position.y,
+              transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, targetposition, smoothing);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private enum State { idle, running, interact, }
+    private State state = State.idle;
+    public Animator anim;
+    public Transform target;
+    public GameObject player1, player2;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown("space"))
+        {
+            if (player1.activeSelf)
+            {
+                player1.gameObject.SetActive(false);
+                player2.gameObject.SetActive(true);
+            }
+            else
+            {
+                player1.gameObject.SetActive(true);
+                player2.gameObject.SetActive(false);
+            }
+        }
+        if (change != Vector3.zero)
+        {
+            moveCharacter();
+        }
+        else
+        {
+            state = State.idle;
+        }
+        anim.SetInteger("state", (int)state);
+        Debug.Log(state);
+
+    }
+
+    void moveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime);
+        float hdireccition = Input.GetAxis("Horizontal");
+        if (hdireccition < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+            state = State.running;
+        }
+        else if (hdireccition > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+            state = State.running;
+        }
+    }
+}
